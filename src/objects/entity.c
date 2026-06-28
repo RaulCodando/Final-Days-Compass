b5320e84f700cf9187b8fcd8029ad8c1ee740911
Raul Nascimento <raul.nascimento@edge.ufal.br>
2026-06-28 15:09:24 -0300

    Removes gitkeep from the physics directory and updates the entity to support colliders

--- src/objects/entity.c
+++ src/objects/entity.c
@@ -10,11 +10,33 @@ Entity* entity_create(ObjectIDs id, Sprite *sprite, int health, int standard_att
     entity->speed = speed;
     entity->x_pos = x_pos;
     entity->y_pos = y_pos;
+    entity->collider = NULL;
+    entity->collider_width = 0;
+    entity->collider_height = 0;
+    entity->offset_x = 0;
+    entity->offset_y = 0;
     return entity;
+}
+
+void entity_init_collider(Entity* entity, float collider_width, float collider_height, float offset_x, float offset_y){
+    if(!entity) return;
+    
+    entity->collider = collider_create(entity->x_pos + offset_x, entity->y_pos + offset_y, collider_width, collider_height);
+    if(!entity->collider) return;
+
+    entity->collider_width = collider_width;
+    entity->collider_height = collider_height;
+    entity->offset_x = offset_x;
+    entity->offset_y = offset_y;
 }
 
 void entity_destroy(Entity *entity){
     if(!entity) return;
     game_object_destroy(&(entity->base));
+
+    if(entity->collider){
+        collider_destroy(entity->collider);
+        entity->collider = NULL;
+    }
     free(entity);
 }
--- src/objects/entity.h
+++ src/objects/entity.h
@@ -2,6 +2,7 @@
 #define ENTITY_H
 
 #include "game_object.h"
+#include "../physics/collision.h"
 
 typedef struct Entity {
     GameObject base;
@@ -11,9 +12,16 @@ typedef struct Entity {
     float speed;
     float x_pos;
     float y_pos;
+
+    Collider* collider;
+    float collider_width;
+    float collider_height;
+    float offset_x;
+    float offset_y;
 } Entity;
 
 Entity *entity_create(ObjectIDs id, Sprite *sprite, int health, int standard_attack, float speed, float x_pos, float y_pos);
+void entity_init_collider(Entity* entity, float collider_width, float collider_height, float offset_x, float offset_y);
 void entity_destroy(Entity *entity);
 
 #endif //ENTITY_H
