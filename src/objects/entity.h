b84d6cbea2b11ffed3f460fe7f4112ee3240fb9e
Raul Alves Raul <ran@ic.ufal.br>
2026-07-12 15:40:52 -0300

    Updates entity_move_and_collide to accept Vector for colliders

--- src/objects/entity.h
+++ src/objects/entity.h
@@ -27,7 +27,7 @@ typedef struct Vector Vector;
 
 Entity *entity_create(ObjectIDs id, Sprite *sprite, int health, int standard_attack, float speed, float x_pos, float y_pos);
 void entity_init_collider(Entity* entity, float collider_width, float collider_height, float offset_x, float offset_y);
-void entity_move_and_collide(Entity* entity, Map* map, SolidTileIDs* solid_tile_ids, Collider* colliders, int collider_count, Vector* entities);
+void entity_move_and_collide(Entity* entity, Map* map, SolidTileIDs* solid_tile_ids, Vector* colliders, Vector* entities);
 void entity_destroy(Entity *entity);
 
 #endif //ENTITY_H
