a25065a83380eb723430986c55d237a4cd0156ee
Raul Nascimento <raul.nascimento@edge.ufal.br>
2026-08-01 21:47:27 -0300

    Updates window_manager module

--- src/managers/window_manager.c
+++ src/managers/window_manager.c
@@ -2,23 +2,39 @@
 #include "../graphics/renderer.h"
 #include "../graphics/camera.h"
 #include "../core/settings.h"
+#include "../core/settings.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-bool init_window(Camera **camera, Renderer **renderer, float camera_x, float camera_y, float dead_zone_percentage){
-    if(!camera || !renderer) return false;
+bool init_window(Camera **camera, Renderer **renderer, SDL_Window **window, float camera_x, float camera_y, float dead_zone_percentage) {
+    if (!camera || !renderer || !window) return false;
+
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        fprintf(stderr, "Erro ao inicializar SDL: %s\n", SDL_GetError());
+        return  false;
+    }
     
+    *window = SDL_CreateWindow("Game", SCREEN_WIDTH * RENDER_SCALE, SCREEN_HEIGHT * RENDER_SCALE, SDL_WINDOW_FULLSCREEN);
+    if (*window == NULL) { 
+        fprintf(stderr, "Failed to create window: %s\n", SDL_GetError());
+        return false;
+    }
+
     *camera = camera_create(camera_x, camera_y, dead_zone_percentage, SCREEN_WIDTH, SCREEN_HEIGHT);
-    if(*camera == NULL){ 
-        printf("Failed to create camera\n");
+    if (*camera == NULL) { 
+        fprintf(stderr, "Failed to create camera\n");
+        SDL_DestroyWindow(*window);
+        *window = NULL;
         return false;
     }
     
-    *renderer = renderer_create(SCREEN_WIDTH, SCREEN_HEIGHT);
-    if(*renderer == NULL){
+    *renderer = renderer_create(*window, SCREEN_WIDTH, SCREEN_HEIGHT);
+    if (*renderer == NULL) {
+        fprintf(stderr, "Failed to create renderer\n");
         camera_destroy(*camera);
         *camera = NULL;
-        printf("Failed to create renderer\n");
+        SDL_DestroyWindow(*window);
+        *window = NULL;
         return false;
     }
 
--- src/managers/window_manager.h
+++ src/managers/window_manager.h
@@ -2,10 +2,11 @@
 #define WINDOW_MANAGER_H
 
 #include <stdbool.h>
+#include <SDL3/SDL.h>
 
 typedef struct Camera Camera;
 typedef struct Renderer Renderer;
 
-bool init_window(Camera **camera, Renderer **renderer, float camera_x, float camera_y, float dead_zone_percentage);
+bool init_window(Camera **camera, Renderer **renderer, SDL_Window **window, float camera_x, float camera_y, float dead_zone_percentage);
 
 #endif /* WINDOW_MANAGER_H */
