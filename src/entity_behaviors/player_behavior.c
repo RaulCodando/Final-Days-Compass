6c2c813c28690a17a38b1275176d7abe64bbcf2a
Raul Nascimento <raul.nascimento@edge.ufal.br>
2026-08-01 21:47:54 -0300

    Updates player_behavior.c

--- src/entity_behaviors/player_behavior.c
+++ src/entity_behaviors/player_behavior.c
@@ -5,12 +5,11 @@
 
 void player_update(Entity* self, Game* game){
     if(!self || !game) return;
-    if(&game->commands == NULL) return;
 
-    if (game->commands.move_left.active) self->vel_x = -2.0f * self->speed * game->delta_time;
-    if (game->commands.move_right.active) self->vel_x = 2.0f * self->speed * game->delta_time;
-    if (game->commands.move_up.active) self->vel_y = -1.0f * self->speed * game->delta_time;
-    if (game->commands.move_down.active) self->vel_y = 1.0f * self->speed * game->delta_time;
+    if (game->commands.move_left.active) self->vel_x = -4.0f * self->speed * game->delta_time;
+    if (game->commands.move_right.active) self->vel_x = 4.0f * self->speed * game->delta_time;
+    if (game->commands.move_up.active) self->vel_y = -4.0f * self->speed * game->delta_time;
+    if (game->commands.move_down.active) self->vel_y = 4.0f * self->speed * game->delta_time;
     
     entity_move_and_collide(self, game->map, &game->solid_tile_ids, game->custom_colliders, game->entities);
 }
