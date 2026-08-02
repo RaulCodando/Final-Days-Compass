b6b9df9c3e4ac6f1390dde3bf419ec7974f985d9
Raul Nascimento <raul.nascimento@edge.ufal.br>
2026-08-01 21:47:29 -0300

    Adds test_refactored_sprite module

---
+++ refactoring_tests/graphics_tests/test_refactored_sprite.h
@@ -0,0 +1,10 @@
+#ifndef TEST_REFACTORED_SPRITE_H
+#define TEST_REFACTORED_SPRITE_H
+
+#include <SDL3/SDL.h>
+
+void test_sprite_create_blank(SDL_Renderer *renderer);
+void test_sprite_create(SDL_Renderer *renderer);
+void test_sprite(void);
+
+#endif // TEST_REFACTORED_SPRITE_H
