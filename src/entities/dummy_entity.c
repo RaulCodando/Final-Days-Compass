ad5e8b5861886e37727b5645ae9edb95d341492d
Raul Nascimento <raul.nascimento@edge.ufal.br>
2026-08-02 20:50:48 -0300

    Updates dummy_entity.c

--- src/entities/dummy_entity.c
+++ src/entities/dummy_entity.c
@@ -14,7 +14,7 @@ bool test_entity_spawn(Game *game, float x, float y){
     Entity *test_entity = entity_create(TEST_ENTITY, sprite, NULL, 10.0f, x, y);
     if (!test_entity) return false;
     
-    entity_init_collider(test_entity, 8.0f, 32.0f, 12.0f, 0.0f);
+    entity_init_collider(test_entity, 8.0f, 32.0f, 3.0f, 0.0f);
     test_entity->update_behavior = test_entity_behavior_update;
 
     return manage_entities_add(game, test_entity);
