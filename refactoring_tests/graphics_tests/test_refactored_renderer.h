bfa75a21622b7230b70c8990bf0274e775756c6b
Raul Nascimento <raul.nascimento@edge.ufal.br>
2026-08-01 21:47:28 -0300

    Adds test_refactored_renderer module

---
+++ refactoring_tests/graphics_tests/test_refactored_renderer.h
@@ -0,0 +1,16 @@
+#ifndef TEST_REFACTORED_RENDERER_H
+#define TEST_REFACTORED_RENDERER_H
+
+#include <SDL3/SDL.h>
+#include "../../src/graphics/renderer.h"
+
+Renderer *test_renderer_create(SDL_Window *window);
+void test_renderer_clear(Renderer *renderer);
+void test_renderer_draw(Renderer *renderer);
+void test_renderer_draw_tile(Renderer *renderer);
+void test_renderer_draw_map(Renderer *renderer);
+void test_renderer_present(Renderer *renderer);
+
+void test_renderer();
+
+#endif // TEST_REFACTORED_RENDERER_H
