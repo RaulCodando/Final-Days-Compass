e28d1ee4d537261d237c3c9ecebdf9db47dea655
Raul Nascimento <raul.nascimento@edge.ufal.br>
2026-08-01 21:47:54 -0300

    Updates dummy_entity.c

--- src/entities/dummy_entity.c
+++ src/entities/dummy_entity.c
@@ -8,13 +8,13 @@
 bool test_entity_spawn(Game *game, float x, float y){
     if(!game) return false;
 
-    Sprite *sprite = asset_manager_get(game->asset_manager, TEST_ENTITY, "tests/assets/test_entity_sprite.txt");
+    Sprite *sprite = asset_manager_get(game->renderer->sdl_renderer, game->asset_manager, TEST_ENTITY, "tests/assets/test_entity_sprite.png");
     if(!sprite) return false;
 
     Entity *test_entity = entity_create(TEST_ENTITY, sprite, NULL, 10.0f, x, y);
     if (!test_entity) return false;
     
-    entity_init_collider(test_entity, 4.0f, 8.0f, 2.0f, 0.0f);
+    entity_init_collider(test_entity, 8.0f, 32.0f, 12.0f, 0.0f);
     test_entity->update_behavior = test_entity_behavior_update;
 
     return manage_entities_add(game, test_entity);
