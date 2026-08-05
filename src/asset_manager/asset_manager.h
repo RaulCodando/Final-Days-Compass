010205bdc98872971ca0058783bc873063765beb
Raul Nascimento <raul.nascimento@edge.ufal.br>
2026-08-05 16:11:53 -0300

    Adds asset_manager module

---
+++ src/asset_manager/asset_manager.h
@@ -0,0 +1,15 @@
+#ifndef ASSET_MANAGER_H
+#define ASSET_MANAGER_H
+
+#include "../objects/game_object.h"
+#include <SDL3/SDL.h>
+
+typedef struct AssetManager {
+    Sprite *object_sprites[OBJECT_COUNT];
+} AssetManager;
+
+AssetManager* asset_manager_create();
+void asset_manager_destroy(AssetManager* asset_manager);
+Sprite* asset_manager_get(SDL_Renderer *renderer, AssetManager* asset_manager, ObjectIDs id, const char *filepath);
+
+#endif //ASSET_MANAGER_H
