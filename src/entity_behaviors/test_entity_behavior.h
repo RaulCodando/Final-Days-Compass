512277c082965fc73e03231ff281ce9e88221a76
Raul Nascimento <raul.nascimento@edge.ufal.br>
2026-08-05 16:12:19 -0300

    Deletes test_entity_behavior module

--- src/entity_behaviors/test_entity_behavior.h
+++
@@ -1,9 +0,0 @@
-#ifndef TEST_ENTITY_BEHAVIOR_H
-#define TEST_ENTITY_BEHAVIOR_H
-
-typedef struct Entity Entity;
-typedef struct Game Game;
-
-void test_entity_behavior_update(Entity *self, Game *game);
-
-#endif // TEST_ENTITY_BEHAVIOR_H
