7f645f0971103bd9f1f45abb2ad28c2f6db24430
Raul Nascimento <raul.nascimento@edge.ufal.br>
2026-08-06 09:43:55 -0300

    Deletes test_world_manager module

--- tests/managers_tests/test_world_manager.h
+++
@@ -1,9 +0,0 @@
-#ifndef TEST_WORLD_MANAGER_H
-#define TEST_WORLD_MANAGER_H
-
-void test_init_world(void);
-void test_init_colliders(void);
-void test_add_collider(void);
-void test_world_manager(void);
-
-#endif // TEST_WORLD_MANAGER_H
