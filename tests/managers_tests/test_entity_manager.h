8e7369fcd958b0a850505470ac5fa65b66704ad6
Raul Nascimento <raul.nascimento@edge.ufal.br>
2026-08-06 09:43:46 -0300

    Deletes test_entity_manager module

--- tests/managers_tests/test_entity_manager.h
+++
@@ -1,8 +0,0 @@
-#ifndef TEST_ENTITY_MANAGER_H
-#define TEST_ENTITY_MANAGER_H
-
-void test_init_entities(void);
-void test_add_entity(void);
-void test_entity_manager(void);
-
-#endif // TEST_ENTITY_MANAGER_H
