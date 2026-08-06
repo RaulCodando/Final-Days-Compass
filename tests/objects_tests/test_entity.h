9782690e8493b42b987b1f8b115fd260fa586352
Raul Nascimento <raul.nascimento@edge.ufal.br>
2026-08-06 09:44:07 -0300

    Deletes test_entity module

--- tests/objects_tests/test_entity.h
+++
@@ -1,9 +0,0 @@
-#ifndef TEST_ENTITY_H
-#define TEST_ENTITY_H
-
-void test_entity_create();
-void test_entity_init_collider();
-void test_entity_move_and_collide();
-void test_entity();
-
-#endif //TEST_ENTITY_H
