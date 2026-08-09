e023d874d9014814bbab975dbf66a4cab48bfabf
Raul Nascimento <raul.nascimento@edge.ufal.br>
2026-08-09 15:44:43 -0300

    Updates scenery_element module

--- src/objects/scenery_element.c
+++ src/objects/scenery_element.c
@@ -1,23 +1,26 @@
 #include "scenery_element.h"
 #include <stdlib.h>
 
-SceneryElement *scenery_element_create(ObjectIDs id, float x_pos, float y_pos, Sprite *sprite, float collider_offset_x, float collider_offset_y, float collider_width){
+SceneryElement *scenery_element_create(ObjectIDs id, float x_pos, float y_pos, Sprite *sprite){
     SceneryElement *scenery_element = (SceneryElement*) malloc(sizeof(SceneryElement));
     if(!scenery_element) return NULL;
     scenery_element->base.id = id;
     scenery_element->x_pos = x_pos;
     scenery_element->y_pos = y_pos;
     scenery_element->base.sprite = sprite;
-    scenery_element->collider_offset_x = collider_offset_x;
-    scenery_element->collider_offset_y = collider_offset_y;
-    scenery_element->collider_width = collider_width;
+    scenery_element_set_y_anchor(scenery_element);
     return scenery_element;
 }
 
-float scenery_element_get_y_anchor(const SceneryElement *element) {
-    if (!element) return 0.0f;
-    if (element->base.sprite) return element->y_pos + element->base.sprite->height;
-    return element->y_pos;
+void scenery_element_set_y_anchor(SceneryElement *element){
+    if(!element) return;
+    if(element->base.sprite) element->y_anchor = element->y_pos + element->base.sprite->height;
+    else element->y_anchor = element->y_pos;
+}
+
+void scenery_element_set_y_anchor_at_y_pos(SceneryElement *element, float y_pos){
+    if(!element) return;
+    element->y_anchor = y_pos;
 }
 
 void scenery_element_destroy(SceneryElement *scenery_element){
--- src/objects/scenery_element.h
+++ src/objects/scenery_element.h
@@ -10,13 +10,12 @@ typedef struct SceneryElement{
 
     float x_pos;
     float y_pos;
-    float collider_offset_x;
-    float collider_offset_y;
-    float collider_width;
+    float y_anchor;
 } SceneryElement;
 
-SceneryElement *scenery_element_create(ObjectIDs id, float x_pos, float y_pos, Sprite *sprite, float collider_offset_x, float collider_offset_y, float collider_width);
-float scenery_element_get_y_anchor(const SceneryElement *element);
+SceneryElement *scenery_element_create(ObjectIDs id, float x_pos, float y_pos, Sprite *sprite);
+void scenery_element_set_y_anchor(SceneryElement *element);
+void scenery_element_set_y_anchor_at_y_pos(SceneryElement *element, float y_pos);
 void scenery_element_destroy(SceneryElement *scenery_element);
 
 #endif // SCENERY_ELEMENT_H
