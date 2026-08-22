735f6f9a9002ae793491fb617d51b9f5a78b93b4
Raul Nascimento <raul.nascimento@edge.ufal.br>
2026-08-22 14:25:01 -0300

    Updates menu_hud.c

--- src/ui/menu_hud.c
+++ src/ui/menu_hud.c
@@ -6,15 +6,16 @@
 #include "../core/settings.h"
 
 HudElement *menu_hud_create(SDL_Renderer *renderer, TTF_Font *font) {
-    const char *sprite_paths01[1] = {"assets/sprites/hud_sprites/menu_hud_container_sprite.png"};
-    const char *sprite_paths02[2] = {
-        "assets/sprites/hud_sprites/hud_container_sprite.png",
-        "assets/sprites/hud_sprites/hud_selected_container_sprite.png"
-    };
+    SpriteSheet *hud_container_spritesheet = sprite_sheet_create(renderer, "assets/sprites/hud_sprites/hud_container_spritesheet.png", 136, 120);
+    SpriteSheet *menu_hud_container_spritesheet = sprite_sheet_create(renderer, "assets/sprites/hud_sprites/menu_hud_container_sprite.png", 160, 80);
 
-    HudElement *main_container = hud_create_container(renderer, sprite_paths01, 1, 0, 0, 40, 20, true, true);
-    HudElement *button_container_01 = hud_create_container(renderer, sprite_paths02, 2, 0, 16, 34, 5, true, false);
-    HudElement *button_container_02 = hud_create_container(renderer, sprite_paths02, 2, 0, 44, 34, 5, true, false);
+    Sprite *hud_container_sprite = sprite_create_from_sheet(hud_container_spritesheet, 136, 120);
+    Sprite *menu_hud_container_sprite = sprite_create_from_sheet(menu_hud_container_spritesheet, 160, 80);
+    
+
+    HudElement *main_container = hud_create_container(renderer, menu_hud_container_sprite, 0, 0, 40, 20, true, true);
+    HudElement *button_container_01 = hud_create_container(renderer, hud_container_sprite, 0, 16, 34, 5, true, false);
+    HudElement *button_container_02 = hud_create_container(renderer, hud_container_sprite, 0, 44, 34, 5, true, false);
     
     HudElement *button_text01 = hud_create_text("Continue", 0, 0, true, true, font);
     HudElement *button_text02 = hud_create_text("Exit", 0, 0, true, true, font);
