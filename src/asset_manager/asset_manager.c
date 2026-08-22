a9f753339ff7e4a750b27bd476c2533b07f6a82e
Raul Nascimento <raul.nascimento@edge.ufal.br>
2026-08-22 14:21:25 -0300

    Updates asset_manager module

--- src/asset_manager/asset_manager.c
+++ src/asset_manager/asset_manager.c
@@ -1,9 +1,11 @@
 #include "asset_manager.h"
+#include "../graphics/sprite.h"
 #include <stdlib.h>
 
 AssetManager* asset_manager_create(){
     AssetManager* asset_manager = (AssetManager*)malloc(sizeof(AssetManager));
     for (int i = 0; i < OBJECT_COUNT; i++) {
+        asset_manager->objects_sprite_sheets[i] = NULL;
         asset_manager->object_sprites[i] = NULL;
     }
     return asset_manager;
@@ -15,16 +17,22 @@ void asset_manager_destroy(AssetManager* asset_manager) {
             if (asset_manager->object_sprites[i]) {
                 sprite_destroy(asset_manager->object_sprites[i]);
             }
+            if (asset_manager->objects_sprite_sheets[i]) {
+                sprite_sheet_destroy(asset_manager->objects_sprite_sheets[i]);
+            }
         }
         free(asset_manager);
     }
 }
 
-Sprite *asset_manager_get(SDL_Renderer *renderer, AssetManager *asset_manager, ObjectIDs id, const char *filepath){
+Sprite *asset_manager_get(SDL_Renderer *renderer, AssetManager *asset_manager, ObjectIDs id, const char *filepath, int frame_width, int frame_height){
     if(!asset_manager || id >= OBJECT_COUNT) return NULL;
 
+    if (asset_manager->objects_sprite_sheets[id] == NULL) {
+        asset_manager->objects_sprite_sheets[id] = sprite_sheet_create(renderer, filepath, frame_width, frame_height);
+    }
     if (asset_manager->object_sprites[id] == NULL) {
-        asset_manager->object_sprites[id] = sprite_create(renderer, filepath);
+        asset_manager->object_sprites[id] = sprite_create_from_sheet(asset_manager->objects_sprite_sheets[id], frame_width, frame_height);
     }
     return asset_manager->object_sprites[id];
 }
--- src/asset_manager/asset_manager.h
+++ src/asset_manager/asset_manager.h
@@ -5,11 +5,12 @@
 #include <SDL3/SDL.h>
 
 typedef struct AssetManager {
+    SpriteSheet *objects_sprite_sheets[OBJECT_COUNT];
     Sprite *object_sprites[OBJECT_COUNT];
 } AssetManager;
 
 AssetManager* asset_manager_create();
 void asset_manager_destroy(AssetManager* asset_manager);
-Sprite* asset_manager_get(SDL_Renderer *renderer, AssetManager* asset_manager, ObjectIDs id, const char *filepath);
+Sprite* asset_manager_get(SDL_Renderer *renderer, AssetManager *asset_manager, ObjectIDs id, const char *filepath, int frame_width, int frame_height);
 
 #endif //ASSET_MANAGER_H
